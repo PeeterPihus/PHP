c1b50e783597748a0cb2a8effa44c9f32d00a79a
PeeterPihus <peeter.pihus@khk.ee>
2017-01-11 14:43:22 +0200

    text calss initial changes

---
+++ oop/text.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * User: Peeter
+ * Date: 11.01.2017
+ * Time: 14:22
+ */
+class text
+{//text class begin
+    //class variables =instance variables
+    var $str = '';
+
+    //methods
+    //set text function
+    function setText($s) {
+        $this->str = $s
+    }//setText
+
+    //show text function
+    function show(){
+        echo $this->str.'</br>';
+    }
+}//text class end
+?>
